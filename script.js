740ced664f9829d3219ba4fc4305c58e7b0ca73a
Alex Bernardino <oalexbernardino@gmail.com>
2023-04-25 22:21:15 -0300

    adição de movimentos

--- script.js
+++ script.js
@@ -1,38 +1,122 @@
 const canvas = document.getElementById('game-board');
 const ctx = canvas.getContext('2d');
-
 const tileSize = 20;
 const numRows = canvas.height/tileSize;
-const numCols = canvas.with/tileSize;
+const numCols = canvas.width/tileSize;
+// const scoreElement = document.getElementById('score');
 
-let snake = [{ x:10, y10 }];
-let food = {x:15, y:15 };
+let snake = [{ x:10, y:10 }];
+let food = { x:15, y:15 };
 let dx = 0;
 let dy = 0;
+// let score = 0;
+
+document.addEventListener('keydown', (event)=>{
+    if(event.key === 'ArrowUp' && dy === 0){
+        dx = 0;
+        dy = -1;
+    }
+    if(event.key === 'ArrowDown' && dy === 0){
+        dx = 0;
+        dy = 1;
+    }
+    if(event.key === 'ArrowLeft' && dx === 0){
+        dx = -1;
+        dy = 0;
+    }
+    if(event.key === 'ArrowRight' && dx === 0){
+        dx = 1;
+        dy = 0;
+    }        
+})
 
 function main (){
-    setTimeout(()=> {
-        //ToDo: desenhar a minha cobra
+    setTimeout(() => {
+        moveSnake()
+        checkSnakeCollision()
+        checkFoodCollision()
+        clearCanvas()
         drawSnake()
-        //ToDo: desenhar o alimento
-        drawnFood()
-        //ToDo: movimentação da cobra
-        //ToDo: checar se a cobra bateu em algo
-        //ToDo: checar se a cobra bateu na comida
+        drawFood()
+        // updateScore()
         main()
     }, 100)
 }
+
 
 function drawSnake(){
     ctx.fillStyle = 'green'
     snake.forEach((segment) => {
         ctx.fillRect(segment.x*tileSize, segment.y*tileSize, tileSize, tileSize);
-        ctx.strokeStyle= 'black';
-        ctx.strokeRect(segment.x*tileSize, segment.y*tileSize, tileSize, tileSize)
+        ctx.strokeStyle = 'black';
+        ctx.strokeRect(segment.x*tileSize, segment.y*tileSize, tileSize, tileSize);
     })
 }
 
 function drawFood(){
     ctx.fillStyle = 'red';
-    ctx.fillRect/food
+    ctx.fillRect(food.x * tileSize, food.y * tileSize, tileSize, tileSize)
 }
+
+function moveSnake(){
+    const headSnake = {x: snake[0].x + dx, y: snake[0].y + dy }
+    snake.unshift(headSnake)
+    snake.pop()
+}
+
+function checkFoodCollision(){
+    const positionX = snake[0].x === food.x;
+    const positionY = snake[0].y === food.y;
+    if(positionX && positionY){
+        generateFood();
+        growSnake();
+        // score++;
+    }
+}
+
+function growSnake (){
+    const tailSnake = {...snake[snake.length -1 ]}
+    snake.push(tailSnake)
+}
+
+function generateFood(){
+    food.x = Math.floor(Math.random() * numCols)
+    food.y = Math.floor(Math.random() * numRows)
+}
+
+
+function checkSnakeCollision(){
+    if(
+        snake[0].x < 0 ||
+        snake[0].x >= numCols ||
+        snake[0].y < 0 ||
+        snake[0].y >= numRows
+    ){
+        resetGame()
+    }
+    for(let i = 1; i < snake.length; i++){
+        const positionX = snake[i].x === snake[0].x;
+        const positionY = snake[i].y === snake[0].y;
+        if(positionX && positionY
+            ){
+            resetGame()
+        }
+    }
+}
+
+function resetGame() {
+    snake = [{ x:10, y:10 }];
+    food = { x:15, y:15 };
+    dx = 0;
+    dy = 0;
+}
+
+function clearCanvas(){
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+}
+
+// function updateScore()
+
+main()
